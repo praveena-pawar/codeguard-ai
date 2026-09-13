2cbe433c69b67345808652d737a293c4ef05dcc6
praveena-pawar <praveena.dev02@gmail.com>
2026-09-13 12:20:52 +0530

    Allow Vercel frontend origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://codeguard-ai-ten.vercel.app", ],
+    allow_origins=[
+    "http://localhost:5173",
+    "https://codeguard-ai-ten.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
